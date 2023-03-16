e99fb715de19a184a1ee73a44ec83afe32ee1216
pat naj <test@example.com>
2023-03-16 16:13:16 +0100

    lab 4

--- lab2_model.py
+++ lab2_model.py
@@ -1,42 +1,53 @@
-#%%
+# %%
 import torch
+import os
+
 
 class TinyModel(torch.nn.Module):
 
-    def __init__(self, x = 1024):
+    def __init__(self, x=1024, input_range=1):
         super(TinyModel, self).__init__()
         self.activation = torch.nn.LeakyReLU()
-        # self.activation = torch.nn.ReLU()
-        self.linear1 = torch.nn.Linear(1, x)
+        self.linear1 = torch.nn.Linear(input_range, x)
         self.linear2 = torch.nn.Linear(x, x)
-        self.linear3 = torch.nn.Linear(x, 1)
-        
+        self.linear3 = torch.nn.Linear(x, input_range)
+
     def forward(self, x):
         x = self.linear1(x)
         x = self.activation(x)
         x = self.linear2(x)
         x = self.activation(x)
         x = self.linear3(x)
-        # x = self.activation(x)
         return x
 
-if __name__=='__main__':
-    tinymodel = TinyModel()
+    def save(self, patch='model.pt'):
+        torch.save(self.state_dict(), patch)
+        print("Model saved: %s"%patch)
 
-    print('The model:')
-    print(tinymodel)
+    def load(self, patch='model.pt'):
+        try:
+            self.load_state_dict(torch.load(patch))
+            print("Model saved: %s"%patch)
+        except Exception as e:
+            print("Error: %s"%e)
 
-    # print('\n\nModel params:')
-    # for param in tinymodel.parameters():
-    #     print(param)
 
-    print("Model params: %i"%sum([param.nelement() for param in tinymodel.parameters()]))
+    def info(self):
+        print(self)
+        print("Params: %i" % sum([param.nelement()
+                                  for param in model.parameters()]))
 
-        
-    #test
-    x = torch.rand(10, 1)
+
+if __name__ == '__main__':
+    input_range = 2
+    model = TinyModel(input_range=input_range)
+    model.info()
+
+    # test
+    x = torch.rand(10, input_range)
     print("x = ", x.size())
-    y = tinymodel(x)
+    y = model(x)
     print("y = ", x.size())
-    
+
 # %%
+
